e37be9b6b901abbb8a1acebe9d83affb15191207
Pavel Mayboroda <Successbloger1@gmail.com>
2019-01-22 16:12:24 +0200

    watchObj implemented

---
+++ modules/watchobj.js
@@ -0,0 +1,27 @@
+
+function watchObj(el, func){
+
+    return new Proxy(el, {
+
+        set(target, name, value){
+
+            target[name] = value;
+            func(name, value);
+            return true;
+        },
+
+        get(target, name){
+
+            switch (typeof target[name]) {
+                case 'object':
+                    return watchObj(target[name], func);
+                case 'function':
+                    return target[name].bind(target);
+                default:
+                    return target[name];
+            }
+        }
+    });
+};
+
+export default watchObj;
