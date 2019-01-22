f997d4962ac14c1d6480d3b36e05f5873905fe51
Pavel Mayboroda <Successbloger1@gmail.com>
2019-01-22 16:11:38 +0200

    some change in EmailParser

--- modules/emailparser.js
+++ modules/emailparser.js
@@ -2,28 +2,22 @@ export default class {
 
     constructor(email){
         this.email = email;
+    }
 
-        Object.defineProperty(this, 'isCorrect', {
-            get() {
-                const _regExp = /^[a-zа-я0-9.!#$%&'*+-/=?^_`{|}~-]+@[a-zа-я0-9-]+(?:\.[a-zа-я0-9-]+)+$/i;
-                return _regExp.test(this.email);
-            }
-        });
+    get isCorrect(){
+        const _regExp = /^[a-zа-я0-9.!#$%&'*+-/=?^_`{|}~-]+@[a-zа-я0-9-]+(?:\.[a-zа-я0-9-]+)+$/i;
+        return _regExp.test(this.email);
+    }
 
-        Object.defineProperty(this, 'name', {
-            get() {
-                if (!this.isCorrect) return null;
+    get name(){
+        if (!this.isCorrect) return null;
 
-                return this.email.match(/(.+)@/)[1];
-            }
-        })
+        return this.email.match(/(.+)@/)[1];
+    }
 
-        Object.defineProperty(this, 'domain', {
-            get() {
-                if (!this.isCorrect) return null;
+    get domain() {
+        if (!this.isCorrect) return null;
 
-                return this.email.match(/@(.+)/)[1];
-            }
-        })
+        return this.email.match(/@(.+)/)[1];
     }
 }
